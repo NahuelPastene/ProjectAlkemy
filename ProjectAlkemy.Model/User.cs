2df083a32b049d5d2dede825a75dcb7fd4b37815
Nahuel <nahuelpastene@gmail.com>
2021-02-27 20:02:20 -0300

    Agregados display a Users, index de user funcionando y layout cambiado para Users

--- ProjectAlkemy.Model/User.cs
+++ ProjectAlkemy.Model/User.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjectAlkemy.Model
 {
     public class User : Entity
     {
+        [DisplayName("First Name")]
         public string FirstName { get; set; }
+        [DisplayName("Last Name")]
         public string LastName { get; set; }
+        [DisplayName("DNI")]
         public int Dni { get; set; }
+        [DisplayName("Active")]
         public bool Active { get; set; }
+        [DisplayName("Is Teacher")]
         public bool IsTeacher { get; set; }
     }
 }
